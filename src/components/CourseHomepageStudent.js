7c86b9f906e34f406463a7cf224fe37f6fcd5efb
maxban <zihan.ban@sv.cmu.edu>
2019-12-01 16:31:12 -0800

    to rebase from jiarui

--- src/components/CourseHomepageStudent.js
+++ src/components/CourseHomepageStudent.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import "./Style/Signup.scss";
 import Container from "@material-ui/core/Container";
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -10,9 +10,42 @@ import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Box from '@material-ui/core/Box';
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <Typography
+        component="div"
+        role="tabpanel"
+        hidden={value !== index}
+        id={`vertical-tabpanel-${index}`}
+        aria-labelledby={`vertical-tab-${index}`}
+        {...other}
+      >
+        <Box p={3}>{children}</Box>
+      </Typography>
+    );
+  }
+  
+  TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+  };
+  
+  function a11yProps(index) {
+    return {
+      id: `vertical-tab-${index}`,
+      'aria-controls': `vertical-tabpanel-${index}`,
+    };
+  }
 
 class CourseHomepageStudent extends Component {
     constructor(props) {
@@ -21,8 +54,8 @@ class CourseHomepageStudent extends Component {
 
     render() {
         return (
-            <div style = {pageStyle}>
-                <div style = {headerStyle}>
+            <div style={pageStyle}>
+                <div style={headerStyle}>
                     <h1>Class Helper <i className="fas fa-graduation-cap" ></i></h1>
                 </div>
                 <Container component="main" maxWidth="xs" style={containerStyle}>
@@ -31,11 +64,9 @@ class CourseHomepageStudent extends Component {
                         <Typography component="h1" variant="h5">
                             Course Homepage
                         </Typography>
-                        
-                    </div>
-                </Container>
-            </div>
 
+                    </div>
+            
         )
     }
 }
@@ -45,15 +76,15 @@ const pageStyle = {
 }
 
 const headerStyle = {
-    fontSize : "30px",
-    textAlign : "center"
+    fontSize: "30px",
+    textAlign: "center"
 }
 
 const containerStyle = {
-    marginTop : "10%"
+    marginTop: "10%"
 }
 
 const buttonStyle = {
-    marginTop : "10px"
+    marginTop: "10px"
 }
 export default CourseHomepageStudent;
