3aee60a7af281fb193072dacd92e3d40d2a55b0f
seansio1995 <chufan.xiao@west.cmu.edu>
2019-12-01 22:35:26 -0800

    fix question deletion not match error

--- src/components/CreateQuiz.js
+++ src/components/CreateQuiz.js
@@ -5,27 +5,35 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Button from "@material-ui/core/Button";
 import FormControl from '@material-ui/core/FormControl';
 import Box from "@material-ui/core/Box";
+import { v4 } from 'uuid'
 
 class CreateQuiz extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            answers : ["Both", "Need", "Feature"]
+            answers : [{text : "Both", id : v4()},
+                {text : "Need", id : v4()},
+                {text: "Feature", id : v4()}]
         }
         this.deleteQuestion = this.deleteQuestion.bind(this);
     }
 
     createQuestion = () => {
         let currAns = this.state.answers;
-        currAns.push("");
+        currAns.push({
+            text : "",
+            id : v4()
+        });
         this.setState({
             answers : currAns
         })
     }
 
-    deleteQuestion = (i) => {
-        let currentAns = this.state.answers;
-        currentAns.splice(i,1);
+    deleteQuestion = (id) => {
+        console.log(id);
+        let currentAns = this.state.answers.filter((answer) => {
+            return answer.id != id;
+        });
         this.setState({
             answers : currentAns
         })
@@ -33,7 +41,7 @@ class CreateQuiz extends Component {
 
     render() {
         let answerItems = this.state.answers.map((answer,i) => {
-            return <div key = {i} style={{marginTop : "20px"}}>
+            return <div key = {answer.id} style={{marginTop : "20px"}}>
                 <Box display="flex" flexDirection="row" p={1} m={1} >
                     <Box>
                         <h3>Choice {i+1}</h3>
@@ -43,7 +51,7 @@ class CreateQuiz extends Component {
                             required
                             id="standard-required"
                             label="Required"
-                            defaultValue={answer}
+                            defaultValue={answer.text}
                             margin="normal"
                         />
                     </Box>
@@ -55,7 +63,7 @@ class CreateQuiz extends Component {
                             type="submit"
                             variant="contained"
                             color="secondary"
-                            onClick={() => this.deleteQuestion(i)}
+                            onClick={() => this.deleteQuestion(answer.id)}
                         > Delete </Button>
                     </Box>
                 </Box>
